ee6eb198fb4440cc010ceff78c992a1c38d4196a
Cosmobonf <cosmoaraujo525@gmail.com>
2025-04-26 23:30:50 -0300

    config

---
+++ firewall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
